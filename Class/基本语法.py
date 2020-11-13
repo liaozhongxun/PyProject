9efa1598a64a2b29be4c737e474150fedb6a3df7
liaozhongxun <869664233@qq.com>
2020-11-13 20:29:58 +0800

    魔法方法

--- Class/基本语法.py
+++ Class/基本语法.py
@@ -1,4 +1,7 @@
 class Student:
+    #这个属性直接定义在类里,是一个元组,用来规定对象可以存在的属性
+    __slots__ = ('name','height','age','props')
+
     def __init__(self,name,height,age):
         self.name = name
         self.height = height
@@ -10,13 +13,44 @@ class Student:
     def eat(self):
         print('正在吃东西')
 
+    '''
+    魔法方法 类里特殊的一些方法
+        不需要手动调用，在合适时机自动调用, 格式 __xxx__
+        可以直接调用 s1.__repr__()
+        方法名都是系统规定好的,合适时机才会自动调用
+            如:
+            __init__(): 在创建对象时自动调用
+            __del__(self): 对象销毁时自动调用 如程序运行完或del s1就能触发
+            __str__(self): || __repr__(self): 打印对象s1 的时候默认调用这个,返回一个地址,__str__优先级高
+            __call__(self,*args,**kwargs): 当对象被当做函数调用是触发  s1()
+            __eq__(self,other):   self 等号左边, other 等号右边
+            
+    '''
+
+    def __str__(self):
+        return "姓名:{},年龄:{}".format(self.name,self.age)
+
+    def __repr__(self):
+        return "repr"
+
+
+
 # Student() 会自动调用类的 __init__方法
 # 使用Student创建了两个实力对象 s1和s2
 # 两个实例都有name,age,height属性和 run与eat方法
 s1 =Student('小米','1.78',18)
 s2 =Student('小丽','2.78',18)
-s1.run()
 
+s1.run()
+print(s1)
+
+# is判断是否为同一个内存 False
+print(s1 is s2)
+
+# == 调用对象__eq__()
+
+# 默认 是s1.paops  如果属性不存在会自动添加，称动态属性
+s1.props = "props"
 
 '''
 Student('小丽','2.78',18) 过程如图一   
